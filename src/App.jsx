e033e84fc180f41ba6e81e0d1f850fb75c55e794
AlhanHussain <143873845+AlhanHussain@users.noreply.github.com>
2024-07-09 14:28:24 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Pages/Home/Home"
+import { Route,Routes } from "react-router-dom"
+import Video from "./Pages/Video/Video"
+
+
+const App = () => {
+
+  const [sidebar, setSideBar]  = useState(true);
+
+
+
+  return (   // used (youtube data api sit) services for api
+    <div>
+      <Navbar setSideBar={setSideBar}/>
+      <Routes>
+        <Route path='/' element={<Home sidebar={sidebar}/>}/>
+        <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,12 @@
+export const API_KEY = 'AIzaSyDOC1zZtLNjY5kK0bC9k8DSdRvMBf9ga88';
+
+
+export const value_converter =(value)=>{
+   if(value>=1000000){
+    return Math.floor(value/1000000) + "M";
+   }else if(value>=1000){
+    return Math.floor(value/1000) + "k";
+   }else{
+    return value
+   }
+}
